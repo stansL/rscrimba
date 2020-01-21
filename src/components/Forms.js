848eca4051014fad2cfc818fd55c7dc72ee1a33b
Stanslaus <mstans8@gmail.com>
2020-01-21 06:12:27 +0300

    Update sections on react forms and the rules on controlled components

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -5,9 +5,17 @@ class Forms extends React.Component {
         super(props);
         this.state = {
             firstName: '',
-            lastName: ''
-
+            lastName: '',
+            isFriendly: false,
+            gender: 'Male',
+            textInput:'',
+            favFruit:''
         }
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        alert("Submitted the Form");
     }
 
     processInput = (event) => {
@@ -25,14 +33,40 @@ class Forms extends React.Component {
         // });
     }
 
+    // Take a look at the Formik library
+
     render() {
         return (
-            <div>
+            <form onSubmit={this.handleSubmit}> 
                 <input name="firstName" type='text' value={this.state.firstName} placeholder="First Name" onChange={this.processInput} />
                 <input name="lastName" type='text' value={this.state.lastName} placeholder="Last Name" onChange={this.processInput} />
                 <p>{this.state.firstName} {" "} {this.state.lastName}</p>
-            </div>
+                <textarea value={this.state.value} onChange={this.processInput} name ="textInput" />
+                <br />
+
+                <label>
+                    <input type="checkbox" checked={this.state.isFriendly} onChange={this.processInput} name="isFriendly" /> Some Tst
+                </label>
+                <br />
+                <label>
+                    <input type="radio" checked={this.state.gender === "Female"} onChange={this.processInput} name="gender" value="Female" /> Female
+                </label>
+                <label>
+                    <input type="radio" checked={this.state.gender === "Male"} onChange={this.processInput} name="gender" value = "Male"/> Male
+                </label> <br />
+
+                <label>
+                    Pick your favorite flavor:
+                    <select value={this.state.favFruit} onChange={this.processInput} name = "favFruit">
+                        <option value="grapefruit">Grapefruit</option>
+                        <option value="lime">Lime</option>
+                        <option value="coconut">Coconut</option>
+                        <option value="mango">Mango</option>
+                    </select>
+                </label> <br />
+                <button>Submit</button>
+            </form>
         );
-    }
+    } 
 }
 export default Forms;
